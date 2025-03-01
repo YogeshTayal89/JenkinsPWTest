b312b0960032f629eb97cc96e1d95d6277a7f999
yogesh-tayal-igt <149561897+yogesh-tayal-igt@users.noreply.github.com>
2025-03-01 21:55:51 +0530

    tc 4

--- tests/jenkins.spec.ts
+++ tests/jenkins.spec.ts
@@ -23,7 +23,7 @@ test('Test Case 3', async ({ page }) => {
   await page.goto('https://google.com');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("GoogleWrong");
+  await expect(page).toHaveTitle("Google");
   console.log("Ending  Test case 3")
 });
 
